23ef35bad3aa1c14bbc99ae5d767e235b7fabe6a
Грибовская Яна <gribovskaya166@gmail.com>
2025-12-02 22:18:00 +0300

    feat: добавлено приветствие из ветки feature

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello from main!");
+            Console.WriteLine("А это уже из ветки feature/greeting");
+            Console.ReadKey();
         }
     }
 }
